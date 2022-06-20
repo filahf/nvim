5f8b1b43ebdba5189e472b5efc0a43b88ca85b09
Filip Åhfelt <filip.ahfelt@devoteam.com>
2022-06-20 17:42:43 +0200

    auto update nvim tree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -15,6 +15,9 @@ nvim_tree.setup {
     enable = true,
     update_cwd = true,
   },
+  filesystem_watchers = {
+    enabled = true,
+  },
   renderer = {
     root_folder_modifier = ":t",
     icons = {
