b26e6c2ded0f65b300f7020b5275190edceffd27
Filip Åhfelt <filip.ahfelt@devoteam.com>
2023-02-09 15:49:13 +0100

    fix illuminate

--- lua/user/plugins/illuminate.lua
+++ lua/user/plugins/illuminate.lua
@@ -4,6 +4,10 @@ return {
   opts = { delay = 200 },
   config = function(_, opts)
     require("illuminate").configure(opts)
+    vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
+
     vim.api.nvim_create_autocmd("FileType", {
       callback = function()
         local buffer = vim.api.nvim_get_current_buf()
