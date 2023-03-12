bc6322f3c7d632910cdae44225fba84652e567a0
Filip Åhfelt <filip.ahfelt@devoteam.com>
2023-03-12 20:01:52 +0100

    fix: autopairs

--- lua/user/plugins/autopairs.lua
+++ lua/user/plugins/autopairs.lua
@@ -1,24 +1,33 @@
 return {
-  "windwp/nvim-autopairs",
-  event = "BufEnter",
-  config = function()
-    require("nvim-autopairs").setup({
-      check_ts = true,
-      ts_config = {
-        lua = { "string", "source" },
-        javascript = { "string", "template_string" },
-      },
-      fast_wrap = {
-        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-        end_key = "$",
-        keys = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma = true,
-        highlight = "PmenuSel",
-        highlight_grey = "LineNr",
-      },
-    })
+  {
+    "windwp/nvim-autopairs",
+    event = "BufEnter",
+    config = function()
+      require("nvim-autopairs").setup({
+        check_ts = true,
+        ts_config = {
+          lua = { "string", "source" },
+          javascript = { "string", "template_string" },
+        },
+        fast_wrap = {
+          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+          end_key = "$",
+          keys = "qwertyuiopzxcvbnmasdfghjkl",
+          check_comma = true,
+          highlight = "PmenuSel",
+          highlight_grey = "LineNr",
+        },
+      })
 
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end,
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "BufEnter",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
