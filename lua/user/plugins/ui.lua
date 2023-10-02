6872cb873f53cbbc1f7442ac9b11368f020602fa
Filip Åhfelt <filip.ahfelt@devoteam.com>
2023-10-02 13:42:14 +0200

    chore: remove nvim notify

--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -24,29 +24,4 @@ return {
       end
     end,
   },
-  {
-    "rcarriga/nvim-notify",
-    keys = {
-      {
-        "<leader>un",
-        function()
-          require("notify").dismiss({ silent = true, pending = true })
-        end,
-        desc = "Dismiss all Notifications",
-      },
-    },
-    opts = {
-      timeout = 3000,
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.75)
-      end,
-      background_colour = "#000000",
-    },
-    init = function()
-      vim.notify = require("notify")
-    end,
-  },
 }
