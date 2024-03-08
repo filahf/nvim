d3438b639073bc5894a016454001095ee8517272
Filip Åhfelt <dev@filip.mozmail.com>
2024-03-08 23:37:20 +0100

    fix: neovide config

--- init.lua
+++ init.lua
@@ -1,18 +1,19 @@
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
-  vim.o.guifont = "FiraCode Nerd Font Mono:h11"
-  vim.opt.linespace = -2
-  vim.g.neovide_scale_factor = 0.6
+  vim.o.guifont = "JetBrainsMono Nerd Font:h11"
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_transparency = 0.9
+  vim.g.neovide_scale_factor = 1
   vim.g.neovide_padding_top = 0
   vim.g.neovide_padding_bottom = 0
   vim.g.neovide_padding_right = 0
   vim.g.neovide_padding_left = 0
   vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
-  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+  vim.keymap.set("v", "<C-S-C>", '"+y') -- Copy
+  vim.keymap.set("n", "<C-S-V", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<C-S-V>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<C-S-V", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<C-S-V>", '<ESC>l"+Pli') -- Paste insert mode
 end
 -- Setup lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
