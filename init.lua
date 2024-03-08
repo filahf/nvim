4b4957091bf5b77a60c5a0dfd46494d5fc1a31df
Filip Åhfelt <dev@filip.mozmail.com>
2024-03-09 00:02:49 +0100

    style: remove transparency

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   vim.o.guifont = "JetBrainsMono Nerd Font:h11"
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.g.neovide_transparency = 0.9
+  -- vim.g.neovide_transparency = 0.9
   vim.g.neovide_scale_factor = 1
   vim.g.neovide_padding_top = 0
   vim.g.neovide_padding_bottom = 0
