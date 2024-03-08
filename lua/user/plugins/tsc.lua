66cc1228bec2aa2d11dc450280ca06b4d479790c
Filip Åhfelt <dev@filip.mozmail.com>
2024-03-08 23:36:29 +0100

    feat: ts-error-translator

--- lua/user/plugins/tsc.lua
+++ lua/user/plugins/tsc.lua
@@ -8,6 +8,13 @@ return {
     end,
   },
   {
+    "dmmulroy/ts-error-translator.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("ts-error-translator").setup()
+    end,
+  },
+  {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {},
