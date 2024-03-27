9f807930a4919ab8cd6a25db453f815e27fae153
Filip Åhfelt <dev@filip.mozmail.com>
2024-03-27 15:47:22 +0100

    add neovide scale binds

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -14,4 +14,30 @@ if vim.g.neovide then
   vim.keymap.set("v", "<C-S-V>", '"+P') -- Paste visual mode
   vim.keymap.set("c", "<C-S-V", "<C-R>+") -- Paste command mode
   vim.keymap.set("i", "<C-S-V>", '<ESC>l"+Pli') -- Paste insert mode
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-=>",
+    ":lua vim.g.neovide_scale_factor = math.min(vim.g.neovide_scale_factor + 0.1,  3.0)<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-->",
+    ":lua vim.g.neovide_scale_factor = math.max(vim.g.neovide_scale_factor - 0.1,  0.1)<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-+>",
+    ":lua vim.g.neovide_transparency = math.min(vim.g.neovide_transparency + 0.05, 1.0)<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap(
+    "n",
+    "<C-_>",
+    ":lua vim.g.neovide_transparency = math.max(vim.g.neovide_transparency - 0.05, 0.0)<CR>",
+    { silent = true }
+  )
+  vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-)>", ":lua vim.g.neovide_transparency = 0.9<CR>", { silent = true })
 end
