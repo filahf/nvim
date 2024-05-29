c30ff5da057cf5dcfd53e778f1362ca4e7534a2c
Filip Åhfelt <dev@filip.mozmail.com>
2024-05-29 16:44:54 +0200

    migrate to folke comments

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,23 +1,3 @@
 return {
-  { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
-  {
-    "echasnovski/mini.comment",
-    event = "VeryLazy",
-    opts = {
-      hooks = {
-        pre = function()
-          require("ts_context_commentstring.internal").update_commentstring()
-        end,
-      },
-    },
-    config = function()
-      require("mini.comment").setup({
-        options = {
-          custom_commentstring = function()
-            return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-          end,
-        },
-      })
-    end,
-  },
+  { "folke/ts-comments.nvim", opts = {}, event = "VeryLazy" },
 }
